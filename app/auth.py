f2a2a95ffe080c318800dbbc86ad94cc52113295
davidkiama <davidkiama98@gmail.com>
2022-03-11 23:25:40 +0300

    Added a test profile image

--- app/auth.py
+++ app/auth.py
@@ -4,6 +4,11 @@ from flask import Blueprint, render_template
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/')
-def create_blog():
-    pass
+@auth.route('/login')
+def login():
+    return render_template('auth/login.html')
+
+
+@auth.route('/signup')
+def signup():
+    return render_template('auth/signup.html')
