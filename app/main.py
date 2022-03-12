e452dc3aba86263fb4d22d47cd3d0c8f28aea7cc
davidkiama <davidkiama98@gmail.com>
2022-03-12 11:28:57 +0300

    Styled the Authentication files

--- app/main.py
+++ app/main.py
@@ -7,3 +7,8 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/profile')
+def profile():
+    return render_template('profile.html')
---
+++ app/models.py
@@ -0,0 +1,12 @@
+
+from enum import unique
+from flask_login import UserMixin
+from app import db
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    profile_pic = db.Column(db.String(), nullable=False, default='default.jpg')
