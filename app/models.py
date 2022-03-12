e4b4c9ddb47ac371b088049924aefb706070875f
davidkiama <davidkiama98@gmail.com>
2022-03-12 11:49:55 +0300

    Added the blog model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 
 from enum import unique
+from unicodedata import category
 from flask_login import UserMixin
+from sqlalchemy import JSON
 from app import db
 
 
@@ -10,3 +12,14 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     profile_pic = db.Column(db.String(), nullable=False, default='default.jpg')
+    blogs = db.relationship('Blog', backref='user', passive_deletes=True)
+
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.String(1000))
+    category = db.Column(db.String(100))
+    date_posted = db.Column(db.DateTime, default=db.func.now())
+    author = db.Column(db.Interger, db.ForeignKey(
+        'user.id', ondelete='CASCADE'), nullable=False)
