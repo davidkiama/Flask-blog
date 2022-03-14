e2a69cd48030d47f24a51a4a6ca7742439a632b8
davidkiama <davidkiama98@gmail.com>
2022-03-14 12:59:49 +0300

    Test deployment 4

--- app/config.py
+++ app/config.py
@@ -12,7 +12,7 @@ class DevConfig(Config):
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = 'postgresql://ejkzdidlscvspo:6b00bc2c35912951c6340740ad71d3083808d257b133abc308e472aac9dcb557@ec2-3-209-61-239.compute-1.amazonaws.com:5432/dabln469vn7fbe'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://ejkzdidlscvspo:6b00bc2c35912951c6340740ad71d3083808d257b133abc308e472aac9dcb557@ec2-3-209-61-239.compute-1.amazonaws.com:5432/dabln469vn7fbe'
 
 
 config_options = {
